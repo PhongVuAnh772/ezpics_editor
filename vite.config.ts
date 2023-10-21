61bda44e163374a27d7005d863bb5dc01b3092da
unknown <vuanhphong1701@gmail.com>
2023-10-21 13:40:10 +0700

    https config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
+import mkcert from 'vite-plugin-mkcert'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(),mkcert()],
   server: {
     host: 'editor.ezpics.vn',
-    port: 80 
+    port: 80,
+    https: true
   }
 })

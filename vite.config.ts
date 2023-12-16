685817a3da2e5fbe48a964b16b5b86310b610157
unknown <vuanhphong1701@gmail.com>
2023-12-16 11:10:50 +0700

    ssl config

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,19 @@ import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
 import mkcert from 'vite-plugin-mkcert'
 
-// async function getAvailablePort() {
-//   const configPort = 80;
-//   const fallbackPort = 5173;
-
-//   // Check if the preferred port is available
-//   const isPreferredPortAvailable = await getPort({ port: configPort });
-
-//   // If the preferred port is available, use it; otherwise, use the fallback port
-//   return isPreferredPortAvailable ? configPort : fallbackPort;
-// }
-
 export default defineConfig({
   
   plugins: [react(), tsconfigPaths(),mkcert()],
-  // server: {
+  
+  server: {
+
+    host: 'editor.ezpics.vn',
+    port:  5173,
+    https: true,
+  },
+})
+
+// server: {
   //   host: 'editor.ezpics.vn',
   //   port:  80,
   //   https: true
@@ -27,12 +25,3 @@ export default defineConfig({
   //     fs: 'fs/',
   //   },
   // },
-  server: {
-    // 
-        // host: 'editor.ezpics.vn',
-
-    host: 'editor.ezpics.vn',
-    port:  5173,
-    https: false,
-  },
-})

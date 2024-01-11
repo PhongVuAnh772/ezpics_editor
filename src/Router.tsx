2ba97387e63dc28234ba0cef185a572394122cc8
unknown <vuanhphong1701@gmail.com>
2024-01-11 12:58:46 +0700

    ok

--- src/Router.tsx
+++ src/Router.tsx
@@ -70,7 +70,9 @@ function Router() {
               </RequireAuth>
             }
           >
-                  </Route>
+            <Route index  path="sale-collection" element={<SaleCollection />} />
+            <Route path="purchase-collection" element={<PurchaseCollection />} />
+          </Route>
           <Route path="/category/:id" element={<Category />} />
           <Route
             path="/user-information"
